e14ee044d954f2061e2cab016dc9808197e220fa
vganeshpokuru <42999066+vganeshpokuru@users.noreply.github.com>
2019-11-27 11:48:34 +0530

    Update V5008__job_savings_accrual_interest_posting.sql

--- fineract-provider/src/main/resources/sql/migrations/core_db/V5008__job_savings_accrual_interest_posting.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V5008__job_savings_accrual_interest_posting.sql
@@ -17,4 +17,4 @@
 -- under the License.
 --
 
-INSERT INTO `job` (`name`, `display_name`, `cron_expression`, `create_time`, `task_priority`, `group_name`, `previous_run_start_time`, `next_run_time`, `job_key`, `initializing_errorlog`, `is_active`, `currently_running`, `updates_allowed`, `scheduler_group`, `is_misfired`) VALUES ('Post Accrual Interest for Savings', 'Post Accrual Interest for Savings', '0 0 0 1/1 * ? *', now(), 5, NULL, NULL, NULL, NULL, NULL, 1, 0, 1, 0, 0);
+INSERT INTO `job` (`name`, `display_name`, `cron_expression`, `create_time`, `task_priority`, `group_name`, `previous_run_start_time`, `next_run_time`, `job_key`, `initializing_errorlog`, `is_active`, `currently_running`, `updates_allowed`, `scheduler_group`, `is_misfired`) VALUES ('Post Accrual Interest for Savings', 'Post Accrual Interest for Savings', '0 0 22 1/1 * ? *', now(), 5, NULL, NULL, NULL, NULL, NULL, 1, 0, 1, 0, 0);

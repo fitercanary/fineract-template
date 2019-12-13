8a58cfa1ec36a589673914e60935b08960e135cd
deepika <dcsodani@gmail.com>
2019-12-13 16:47:44 +0530

    Revert "When adding charges, specified due date should not be earlier than last transaction date"
    This reverts commit 532a8609

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/domain/SavingsAccount.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/domain/SavingsAccount.java
@@ -1358,7 +1358,7 @@ public class SavingsAccount extends AbstractPersistableCustom<Long> {
         return transactionDate.isAfter(DateUtils.getLocalDateOfTenant());
     }
 
-    boolean isDateBeforeLastTransaction(final LocalDate transactionDate) {
+    private boolean isDateBeforeLastTransaction(final LocalDate transactionDate) {
         return transactionDate.isBefore(retrieveLastActiveTransactionDate());
     }
     

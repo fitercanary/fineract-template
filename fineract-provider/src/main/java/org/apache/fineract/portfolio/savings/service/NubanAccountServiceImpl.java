2fcb683dd9abcdc3e9a06bef020f7917005fb251
Frank Nkuyahaga <franknkuyahaga@Franks-MacBook-Pro.local>
2020-05-11 11:15:27 +0300

    Fixes VS-94: Post overdraft interest to the right account

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/service/NubanAccountServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/service/NubanAccountServiceImpl.java
@@ -18,7 +18,6 @@
  */
 package org.apache.fineract.portfolio.savings.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service

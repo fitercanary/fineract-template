d458ffa3d7037599e0795b2269e33facac52466a
Frank Nkuyahaga <franknkuyahaga@Franks-MacBook-Pro.local>
2020-07-14 13:08:04 +0300

    Fix interest accrual posting job

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/service/SavingsAccountWritePlatformServiceJpaRepositoryImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/savings/service/SavingsAccountWritePlatformServiceJpaRepositoryImpl.java
@@ -1742,7 +1742,7 @@ public class SavingsAccountWritePlatformServiceJpaRepositoryImpl implements Savi
                 .isSavingsInterestPostingAtCurrentPeriodEnd();
         final Integer financialYearBeginningMonth = this.configurationDomainService.retrieveFinancialYearBeginningMonth();
         if (account.getNominalAnnualInterestRate().compareTo(BigDecimal.ZERO) > 0
-                || account.getNominalAnnualInterestRateOverdraft().compareTo(BigDecimal.ZERO) > 0) {
+                || (account.getNominalAnnualInterestRateOverdraft() != null && account.getNominalAnnualInterestRateOverdraft().compareTo(BigDecimal.ZERO) > 0)) {
             final Set<Long> existingTransactionIds = new HashSet<>();
             final Set<Long> existingReversedTransactionIds = new HashSet<>();
             updateExistingTransactionsDetails(account, existingTransactionIds, existingReversedTransactionIds);

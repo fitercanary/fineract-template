c605fd1e3ec968aa8eaf06524e562daeb5ebc68d
Moses Kalema <mkgremo20@gmail.com>
2020-10-06 12:45:15 +0300

    set environment variables to allow jobs

--- docker/setenv.sh
+++ docker/setenv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-JAVA_OPTS="-Dfineract.datasource.host=canary.mysql.fineract -Dfineract.datasource.db=mifosplatform-tenants -Dfineract.datasource.username=root -Dfineract.datasource.password=mysql"
+JAVA_OPTS="-Dfineract.datasource.host=canary.mysql.fineract -Dfineract.datasource.db=mifosplatform-tenants -Dfineract.datasource.username=root -Dfineract.datasource.password=mysql -Dfineract.allowJobs=true"

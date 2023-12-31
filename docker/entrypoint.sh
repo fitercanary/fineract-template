629bce9377faf9ebac213fccc17c1bd09d85b47b
seyi <seyi.obaweya@getir.com>
2023-12-31 19:46:09 +0100

    fscan-24: create configuration files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+envsubst < /usr/local/tomcat/templates/server.xml.tmpl > /usr/local/tomcat/conf/server.xml
+envsubst < /usr/local/tomcat/templates/setenv.sh.tmpl > /usr/local/tomcat/bin/setenv.sh
 keytool -genkey -noprompt -storepass password -keypass password -keyalg RSA -alias tomcat -dname "CN=tomcat" -keystore /usr/share/tomcat.keystore
 
 catalina.sh jpda run
